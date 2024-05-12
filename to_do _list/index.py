f090824652009e3f8ec0157c950a935d36433928
mugane-wahome <muganewahome@gmail.com>
2024-05-12 17:37:35 +0300

    initial commit of to do list using tkinter

---
+++ to_do _list/index.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from tkinter import messagebox
+
+def add_task():
+    task = task_entry.get()
+    if task:
+        tasks_listbox.insert(END, task)
+        task_entry.delete(0, END)
+    else:
+        messagebox.showwarning("Warning", "Task cannot be empty!")
+
+def remove_task():
+    try:
+        index = tasks_listbox.curselection()[0]
+        tasks_listbox.delete(index)
+    except IndexError:
+        pass
+
+def clear_tasks():
+    tasks_listbox.delete(0, END)
+
+root = Tk()
+root.title("To-Do List")
+
+task_entry = Entry(root, width=40)
+task_entry.pack(pady=10)
+
+add_button = Button(root, text="Add Task", command=add_task)
+add_button.pack(pady=5)
+
+remove_button = Button(root, text="Remove Task", command=remove_task)
+remove_button.pack(pady=5)
+
+clear_button = Button(root, text="Clear All Tasks", command=clear_tasks)
+clear_button.pack(pady=5)
+
+tasks_listbox = Listbox(root, width=50)
+tasks_listbox.pack(padx=10, pady=10)
+
+root.mainloop()
